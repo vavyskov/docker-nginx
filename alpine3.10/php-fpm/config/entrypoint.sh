7f9f0a1fb76c8bfaf06a7c933d536f8536cc9641
Tomáš Daněk <danekt@centrum.cz>
2020-04-20 15:26:01 +0200

    envsubst

--- alpine3.10/php-fpm/config/entrypoint.sh
+++ alpine3.10/php-fpm/config/entrypoint.sh
@@ -4,9 +4,13 @@ set -e
 
 ## https://github.com/docker-library/docs/tree/master/nginx#using-environment-variables-in-nginx-configuration
 envsubst '
+    ${PROJECT_HOSTNAME}
     ${NGINX_ROOT}
     ${NGINX_FASTCGI_HOSTNAME}
     ${NGINX_FASTCGI_PORT}
+    ${CERTIFICATE_PATH}
+    ${CERTIFICATE_FILENAME}
+    ${CERTIFICATE_KEYNAME}
 ' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
 
 ## Make the entrypoint a pass through that then runs the docker command (redirect all input arguments)
