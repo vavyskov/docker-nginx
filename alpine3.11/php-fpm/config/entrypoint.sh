423d32854734e9eee8bb70a5f9b1144b79c6ddbd
Tomáš Daněk <danekt@centrum.cz>
2020-05-17 20:00:20 +0200

    Alpine3.11

---
+++ alpine3.11/php-fpm/config/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+## Exit script if any command fails (non-zero status)
+set -e
+
+## https://github.com/docker-library/docs/tree/master/nginx#using-environment-variables-in-nginx-configuration
+envsubst '
+    ${ACCESS_LOG}
+    ${ERROR_LOG}
+    ${PROJECT_HOSTNAME}
+    ${NGINX_ROOT}
+    ${FASTCGI_HOSTNAME}
+    ${FASTCGI_PORT}
+    ${CERTIFICATE_PATH}
+    ${CERTIFICATE_FILENAME}
+    ${CERTIFICATE_KEYNAME}
+' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
+
+## Make the entrypoint a pass through that then runs the docker command (redirect all input arguments)
+exec "$@"
